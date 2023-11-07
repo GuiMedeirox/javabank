8f7c761296a12293239e486f55887ecd364323b0
GuiMedeirox <omaewa9111@gmail.com>
2023-11-06 21:32:32 -0300

    create Client class

--- src/models/Client.java
+++ src/models/Client.java
@@ -6,6 +6,35 @@ public class Client {
     private int number;
     private String owner;
 
+    public float getBalance() {
+        return balance;
+    }
 
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
 
+    public int getAgency() {
+        return agency;
+    }
+
+    public void setAgency(int agency) {
+        this.agency = agency;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
