7afcec85fc66cf00a05420ede1221eea42dba30b
GuiMedeirox <omaewa9111@gmail.com>
2023-12-24 23:11:02 -0300

    feat: composition

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,7 @@ import workingWithEnum.*;
 
 public class Main {
     public static void main(String[] args) {
-        Client c = new Client();
-        Account BA = new BusinessAccount("John Doe");
-        Account CA = new CurrentAccount("Carl Johnson");
-
-        System.out.println(BA.getTypeAccount().getTariff(1000));
-        System.out.println(CA.getTypeAccount().getTariff(1000));
+        System.out.println(TypeAccount.valueOf("PERSONAL_ACCOUNT"));
 
     }
 }
--- src/workingWithEnum/TypeAccount.java
+++ src/workingWithEnum/TypeAccount.java
@@ -1,10 +1,16 @@
 package workingWithEnum;
 
+import java.lang.reflect.Type;
+
 public enum TypeAccount {
     BUSINESS_ACCOUNT("BUSINESS", 0.25f){
         @Override
         public float getTariff(float value){
             return value*0.1f;
+        }
+        @Override
+        public String getTYPE(TypeAccount typeAccount){
+            return typeAccount.TYPE;
         }
     },
     PERSONAL_ACCOUNT("PERSONAL", 0){
@@ -12,10 +18,23 @@ public enum TypeAccount {
         public float getTariff(float value){
             return 0;
         }
+        @Override
+        public String getTYPE(TypeAccount typeAccount){
+            return typeAccount.TYPE;
+        }
     };
 
+    public static TypeAccount findByField(String field){
+        for(TypeAccount typeAccount: values()){
+            if(typeAccount.getTYPE(typeAccount).equals(field)){
+                return typeAccount;
+            }
+        }
+        return null;
+    }
     public final float VALUE;
     public final String TYPE;
+    public abstract  String getTYPE(TypeAccount typeAccount);
     public abstract  float getTariff(float value);
     TypeAccount(String type, float value){
         this.VALUE = value;
